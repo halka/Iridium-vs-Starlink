346cef0439fb713d8efca1058fb33917931afc49
halka <halka@rjch.jp>
2025-07-23 23:30:52 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+import PostalMime from 'postal-mime';
+import xApiPost from '@kworq/x-api-post';
+
+export default {
+	async email(message: any, env: any, ctx: any) {
+		const parser = new PostalMime();
+		const rawEmail = new Response(message.raw);
+		const email = await parser.parse(await rawEmail.arrayBuffer());
+		let from = email.from.address;
+		let body = email.text ?? '';
+		let subject = email.subject ?? '';
+		let bodyLines = body.split(/\r?\n/).filter(line => line.trim() !== '');
+		let lat: string | null = null;
+		let lon: string | null = null;
+		let googlemapsUrl = 'https://maps.google.com/?q=';
+		let text = '';
+
+		if (subject.includes('inReachメッセージ')) {
+			for (const line of bodyLines) {
+				const match = line.match(/Lat\s([0-9.\-]+)\sLon\s([0-9.\-]+)/);
+				if (match) {
+					lat = match[1];
+					lon = match[2];
+					googlemapsUrl += `${lat},${lon}`;
+					text = `${bodyLines[0]}\n${googlemapsUrl}`;
+					break;
+				}
+			}
+		} else {
+			lat = null;
+			lon = null;
+			text = bodyLines[0];
+		}
+
+		// Tweet using x-api-post
+		const config = {
+			X_API_KEY: env.X_API_KEY || '',
+			X_API_SECRET: env.X_API_SECRET || '',
+			X_API_ACCESS_TOKEN: env.X_API_ACCESS_TOKEN || '',
+			X_API_ACCESS_TOKEN_SECRET: env.X_API_ACCESS_TOKEN_SECRET || ''
+		};
+		const xApiClient = new xApiPost(config);
+		await xApiClient.postTweetWithMedia(text);
+		// console.log(text);
+	}
+}
