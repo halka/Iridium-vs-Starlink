8c850be7bdb895ab41744acedda71a395987e877
halka <halka@rjch.jp>
2025-07-25 02:11:56 +0900

    なければありまえんね

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,27 @@ import xApiPost from '@kworq/x-api-post';
 
 export default {
 	async email(message: any, env: any, ctx: any) {
-		const email = await PostalMime.parse(message.raw);
-		let from = email.from.address;
-		let body = email.text ?? '';
-		let subject = email.subject ?? '';
+		const parser = new PostalMime.default();
+		const rawEmail = new Response(message.raw);
+		const email = await parser.parse(await rawEmail.arrayBuffer());
+		const from = email.from.address;
+		const body = email.text ?? '';
+		const subject = email.subject ?? '';
 		let bodyLines = body.split(/\r?\n/).filter(line => line.trim() !== '');
 		let lat: string | null = null;
 		let lon: string | null = null;
 		let googlemapsUrl = 'https://maps.google.com/?q=';
-		let text = '';
-
+;
 		// Check if sender is allowed
-		let allowList = env.allowed;
-		if (!Array.isArray(allowList)) {
-			try {
-				allowList = JSON.parse(allowList);
-			} catch {
-				allowList = [];
-			}
-		}
+		const allowList = JSON.parse(env.allowed);
+
 		if (!allowList.address.includes(from)) {
 			message.setReject(`Sender not allowed: ${from}`);
 			// Sender not allowed, do not post
-			return;
+			return 1;
 		}
+
+		let text:string = bodyLines[0];
 
 		if (subject.includes('inReachメッセージ')) {
 			for (const line of bodyLines) {
@@ -39,11 +36,8 @@ export default {
 					break;
 				}
 			}
-		} else {
-			lat = null;
-			lon = null;
-			text = bodyLines[0];
 		}
+		if (0 < text.length && text.length < 280) {  
 
 		// Tweet using x-api-post
 		const config = {
@@ -56,5 +50,9 @@ export default {
 		await xApiClient.postTweetWithMedia(text).then((response) => console.log(response))
 			.catch((error: any) => console.error("Error posting tweet:", error));
 		console.log(text);
-	}
+	}else {
+		// Return 1 to indicate success
+		console.log(`Failed: ${text} ${text.length} ${from}`);
+		return 1;
+}
 }
